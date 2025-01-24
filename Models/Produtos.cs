20b3fddfbe822670ec9376730ba83d07d602e8b5
Davi Góes Silveira Barros <davisilbarros@gmail.com>
2025-01-23 21:53:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿namespace Projeto_teste.Models
+{
+    public class Produtos
+    {
+        
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public decimal Preco { get; set; }
+        
+    }
+
+}
+
