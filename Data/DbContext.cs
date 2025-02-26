2b315cd0a3d9f18000e7fc4ad661b9b0d995bb67
Davi Goes <davisilbarros@gmail.com>
2025-02-26 20:21:23 -0300

    Versão 26.02.25

---
+++ Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_teste.Models; // Namespace onde estão os modelos
+
+namespace Projeto_teste.Data
+{
+    public class Projeto_testeContext : DbContext
+    {
+        public Projeto_testeContext(DbContextOptions<Projeto_testeContext> options)
+            : base(options)
+        {
+        }
+
+        // Adicione os DbSets para os modelos
+        public DbSet<Produtos> Produtos { get; set; }
+    }
+}
